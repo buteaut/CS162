8bb083151507e8c2c9fce05fc1834273ffd5d718
buteaut <buteaut@oregonstate.edu>
2017-01-28 16:48:47 -0600

    Modified main.cpp to remove memory leak.

--- Lab 3 Buteau Thomas/Lab 3 Buteau Thomas/main.cpp
+++ Lab 3 Buteau Thomas/Lab 3 Buteau Thomas/main.cpp
@@ -16,41 +16,9 @@
 int main() {
 	std::srand(time(NULL));
 
-	//test block for Game class
 	Game *gameDie = new Game();
 	gameDie->menu();
-	/*
-	gameDie->setDie(20, false, 1);
-	gameDie->setDie(20, true, 2);
-	gameDie->play(20);
-	
-	//test block for Die class
-	Die *testDie = new Die(100);
-	int dieTotal = 0;
-	for (int i = 0; i < 20; i++)
-	{
-		int temp = testDie->roll();
-		std::cout << "Die roll " << i << " is: " << temp << std::endl;
-		dieTotal += temp;
-	}
-	std::cout << "The total for the regular die is: " << dieTotal << std::endl;
-	std::cout << std::endl;
-
-	//test block for LoadedDie class
-	Die *funDie = new LoadedDie(20);
-	dieTotal = 0;
-	for (int i = 0; i < 20; i++)
-	{
-		int temp = funDie->roll();
-		std::cout << "LoadedDie roll " << i << " is: " << temp << std::endl;
-		dieTotal += temp;
-	}
-	std::cout << "The total for the loaded die is: " << dieTotal << std::endl;
-	std::cout << std::endl;
-	
-	delete testDie;
-	delete funDie;
 	delete gameDie;
-	*/
+	
 	return 0;
 }
